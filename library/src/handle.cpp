ac6270f32900980a2b5e22790d93782b4df9e704
AndySu12 <andysu12@amd.com>
2024-04-25 03:37:50 +0000

    Add gfx1150 platform

--- library/src/handle.cpp
+++ library/src/handle.cpp
@@ -141,6 +141,10 @@ static Processor getActiveArch(int deviceId)
     {
         return Processor::gfx1102;
     }
+    else if(deviceString.find("gfx1150") != std::string::npos)
+    {
+        return Processor::gfx1150;
+    }
     return static_cast<Processor>(0);
 }
 
